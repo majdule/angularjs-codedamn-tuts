4d7aba3199fe74cf40dd7a01829ae419aa9a0dd8
MAJ1BG <dusan.majstorovic@bosch.com>
2018-02-02 14:13:01 +0100

    redirecting pages

--- controller.js
+++ controller.js
@@ -1,13 +1,14 @@
 var app = angular.module('mainApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
-  $routeProvider.when('/',{
-      template: 'Welcome user!'
-  })
-  .when('/anotherPage', {
-    template: 'Welcome user, again'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/page.html'
+    })
+    .when('/helloUser', {
+      templateUrl: 'pages/hello.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 });
