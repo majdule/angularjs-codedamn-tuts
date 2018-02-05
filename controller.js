b3951530c5f612cc3949fc9d58efa2dd6ae986a5
MAJ1BG <dusan.majstorovic@bosch.com>
2018-02-05 13:12:03 +0100

    login-form-with-mocked-data

--- controller.js
+++ controller.js
@@ -3,12 +3,30 @@ var app = angular.module('mainApp', ['ngRoute']);
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'pages/page.html'
+      templateUrl: 'pages/login.html'
     })
-    .when('/helloUser', {
-      templateUrl: 'pages/hello.html'
+    .when('/dashboard', {
+      resolve: {
+        "check": function ($location, $rootScope) {
+          if (!$rootScope.loggedIn) {
+            $location.path('/');
+          }
+        }
+      },
+      templateUrl: 'pages/dashboard.html'
     })
     .otherwise({
       redirectTo: '/'
     });
 });
+
+app.controller('loginCtrl', function ($scope, $location, $rootScope) {
+  $scope.submit = function () {
+    if ($scope.username == 'admin' && $scope.password == 'admin') {
+      $rootScope.loggedIn = true;
+      $location.path('/dashboard');
+    } else {
+      alert('Wrong credentials!')
+    }
+  }
+})
