2596ba11d4288fd7bc70906d5280284db959cdcb
MAJ1BG <dusan.majstorovic@bosch.com>
2018-02-06 12:47:06 +0100

    http-service working

--- controller.js
+++ controller.js
@@ -1,8 +1,11 @@
 var app = angular.module('mainApp', []);
 
-app.controller('people', function ($scope, $http) {
-  $http.get('/database.json')
-    .success(function (response) {
-      $scope.persons = response.records;
-    });
+app.controller('peopleCtrl', function ($scope, $http) {
+
+  $http.get('https://majdule.github.io/angularjs-codedamn-tuts/database.json').then(function successCallBack(response) {
+    $scope.persons = response.data.records;
+    console.log($scope.persons);
+  }).catch(function onError(response) {
+    console.log("Error occured");
+  })
 });
